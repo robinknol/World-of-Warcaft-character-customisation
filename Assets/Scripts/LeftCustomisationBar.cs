75cf8756e56f1545c0380e1e09ac661057ed7794
CassieIsFunny <28458@ma-web.nl>
2020-11-16 15:34:38 +0100

    Started working on the UI left side

---
+++ Assets/Scripts/LeftCustomisationBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LeftCustomisationBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Standard Numeric Format Specifiers");
+    }
+}
