b8a6fe715404bbc5337845a91dc1992b9d7e5249
CassieIsFunny <28458@ma-web.nl>
2020-11-23 14:27:30 +0100

    Added button click conformation

---
+++ Assets/Scripts/LeftCustomizationBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class LeftCustomizationBar : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+	}
+}
