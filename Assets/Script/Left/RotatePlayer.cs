1e314ab8df60ec4ee408304eac75f703a3d51af2
CassieIsFunny <28458@ma-web.nl>
2020-12-05 15:59:38 +0100

    Added rotation for character

---
+++ Assets/Script/Left/RotatePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    float rotateSpeed = 200;
+
+    private void OnMouseDrag()
+    {
+        float rotateX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
+        float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotateX);
+        transform.Rotate(Vector3.right, rotateY);
+    }
+}
