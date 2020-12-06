e6df08039e35edf80e9123f36fa10f9b7e773570
CassieIsFunny <28458@ma-web.nl>
2020-12-06 20:00:37 +0100

    Added so the character can only rotate around the x axis

--- Assets/Script/Left/RotatePlayer.cs
+++ Assets/Script/Left/RotatePlayer.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class RotatePlayer : MonoBehaviour
 {
-    float rotateSpeed = 200;
+    float rotateSpeed = 900;
 
     private void OnMouseDrag()
     {
         float rotateX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
-        float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+        //float rotateY = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
 
         transform.Rotate(Vector3.up, -rotateX);
-        transform.Rotate(Vector3.right, rotateY);
+        //transform.Rotate(Vector3.right, rotateY);
     }
 }
